20acc97c349a0851ec1ade4815a545fcfd376a12
Cristian <cristianma98@gmail.com>
2022-05-23 14:21:36 +0200

    primo commit

---
+++ src/main/java/it/prova/manytomanyjpamaven/exception/UtenteConRuoliAssociatiException.java
@@ -0,0 +1,12 @@
+package it.prova.manytomanyjpamaven.exception;
+
+public class UtenteConRuoliAssociatiException extends RuntimeException {
+
+	public UtenteConRuoliAssociatiException() {
+	}
+
+	public UtenteConRuoliAssociatiException(String message) {
+		super(message);
+	}
+
+}
